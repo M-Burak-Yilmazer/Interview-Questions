c8e1a84a9e8ffee3401b72dc24b369ba3d823658
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2023-12-08 14:08:08 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <h2>REACT Interview Ouestions And Answers</h2>
+      <h2>FrontEnd Dev Interview Ouestions And Answers</h2>
      
     </div>
   );
